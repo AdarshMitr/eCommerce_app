0b4d9c54e1b92070ac493204547d8fbeaae1ee39
AdarshMitr <adarshkumar019@gmail.com>
2023-07-22 08:01:35 +0530

    adding Products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Shop from './pages/shop/Shop.jsx';
+import Cart from './pages/cart/Cart.jsx';
 
 function App() {
   return (
@@ -8,8 +10,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" />
-          <Route path="/cart" />
+          <Route path="/" element={<Shop/>}/>
+          <Route path="/cart" element={<Cart/>} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div className='shop'>
+        <div>
+            <h1>Adarsh Shop</h1>
+        </div>
+        <div className="products">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Shop
