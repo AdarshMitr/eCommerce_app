7d3608725dc7100d14c26afa8c474d7b3779514b
AdarshMitr <adarshkumar019@gmail.com>
2023-07-22 08:38:24 +0530

    added Products and Shop pages

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import {PRODUCTS} from "../../Products";
+import Product from './Product';
+import './Shop.css';
 function Shop() {
   return (
     <div className='shop'>
-        <div>
+        <div className='shopTitle'>
             <h1>Adarsh Shop</h1>
         </div>
         <div className="products">
-            
+{PRODUCTS.map((product)=>(<Product data={product}/>)
+    
+)}
         </div>
     </div>
   )
